def4ae69f75aa6a122269f7c7c8f0c9537fb11c1
hallfana <hallfana@proton.me>
2024-12-08 19:51:19 +0100

    auto

--- main.c
+++ main.c
@@ -6,31 +6,25 @@
 /*   By: samberna <samberna@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 17:46:48 by hallfana          #+#    #+#             */
-/*   Updated: 2024/12/08 19:49:45 by samberna         ###   ########.fr       */
+/*   Updated: 2024/12/08 19:51:18 by samberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./mlx_linux/mlx.h"
 #include <math.h>
 
-int generate_rgb()
-{
-	static int i = 0;
-	int m_pi = 3.14159265358979323846;
- 
-	int red = (128.0 + sin(i * 2 * m_pi / 1 + 0.0) * 127.0);
-	int green = (128.0 + sin(i * 2 * m_pi / 1 + 2 * m_pi / 3.0) * 127.0);
-	int blue = (128.0 + sin(i * 2 * m_pi / 1 + 4 * m_pi / 3.0) * 127.0);
-
-	i++;
-	if (i >= 256)
-		i = 0;
-	return (red << 16 | green << 8 | blue);
-}
-
 int	create_trgb(int t, int r, int g, int b)
 {
 	return (t << 24 | r << 16 | g << 8 | b);
+}
+
+int generate_rgb()
+{
+	// generate rgb gradient for each pixel of my banner
+	int r = (int)(sin(0.1) * 127 + 128);
+	int g = (int)(sin(0.2) * 127 + 128);
+	int b = (int)(sin(0.3) * 127 + 128);
+	return (create_trgb(0, r, g, b));
 }
 
 int	main(void)
@@ -42,10 +36,9 @@ int	main(void)
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
 	for(int x = 10; x < 600; x++)
 	{
-		int j = generate_rgb();
 		for (int y = 10 ; y < 100 ; y++)
 		{
-			mlx_pixel_put(mlx, mlx_win, x, y, j);
+			mlx_pixel_put(mlx, mlx_win, x, y, generate_rgb());
 		}
 	}
 	mlx_loop(mlx);
