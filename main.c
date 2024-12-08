f4a09170b8fc4fc184e45853a8f068f69ca12a09
hallfana <hallfana@proton.me>
2024-12-08 19:43:34 +0100

    auto

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: samberna <samberna@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 17:46:48 by hallfana          #+#    #+#             */
-/*   Updated: 2024/12/08 19:42:30 by samberna         ###   ########.fr       */
+/*   Updated: 2024/12/08 19:43:32 by samberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,11 +40,12 @@ int	main(void)
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	for(int x = 10; x < 200; x++)
+	for(int x = 10; x < 300; x++)
 	{
+		int j = generate_rgb();
 		for (int y = 10 ; y < 50 ; y++)
 		{
-			mlx_pixel_put(mlx, mlx_win, x, y, generate_rgb(0, 150, 150, 150));
+			mlx_pixel_put(mlx, mlx_win, x, y, j);
 		}
 	}
 	mlx_loop(mlx);
