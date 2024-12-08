142b5577ae95ea79ca1e9e954accb797e0df9237
hallfana <hallfana@proton.me>
2024-12-08 19:52:59 +0100

    auto

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: samberna <samberna@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 17:46:48 by hallfana          #+#    #+#             */
-/*   Updated: 2024/12/08 19:52:17 by samberna         ###   ########.fr       */
+/*   Updated: 2024/12/08 19:52:58 by samberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,13 @@ int	create_trgb(int t, int r, int g, int b)
 
 int generate_rgb()
 {
-	// generate rgb gradient for each pixel of my banner
-	// fix the code i have a grey banner
-
-	int r = (int)(sin(0.1) * 127 + 128);
-	int g = (int)(sin(0.2) * 127 + 128);
-	int b = (int)(sin(0.3) * 127 + 128);
-	return (create_trgb(0, r, g, b));
-	
+	// generate rgb gradient for each pixel of my banner each time i call this function i wil get the next color
+	static int i = 0;
+	int r = (int)(sin(0.3 * i + 0) * 127 + 128);
+	int g = (int)(sin(0.3 * i + 2) * 127 + 128);
+	int b = (int)(sin(0.3 * i + 4) * 127 + 128);
+	i++;
+	return create_trgb(0, r, g, b);
 }
 
 int	main(void)
