fe0ba59a87811917b030fce253981352195e8374
hallfana <hallfana@proton.me>
2024-12-09 00:13:24 +0100

    auto

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: samberna <samberna@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 17:46:48 by hallfana          #+#    #+#             */
-/*   Updated: 2024/12/09 00:13:09 by samberna         ###   ########.fr       */
+/*   Updated: 2024/12/09 00:13:23 by samberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,12 +43,12 @@ t_data gen_rgb_img(void *mlx)
 	t_data img;
 	img.img = mlx_new_image(mlx, 1920, 1080);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	for(int x = 10; x < 500; x+=1)
+	for(int x = 0; x < 500; x+=1)
 	{
 		int rgb = generate_rgb();
 		for (int x2 = 0; x2 < 1; x2++)
 		{
-			for (int y = 10 ; y < 200 ; y++)
+			for (int y = 0 ; y < 200 ; y++)
 			{
 				my_mlx_pixel_put(&img, x + x2, y, rgb);
 			}
