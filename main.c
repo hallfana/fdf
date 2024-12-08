f65b9852dbd7a74c566145669827f03c8055531e
hallfana <hallfana@proton.me>
2024-12-08 20:15:56 +0100

    auto

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: samberna <samberna@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 17:46:48 by hallfana          #+#    #+#             */
-/*   Updated: 2024/12/08 20:15:42 by samberna         ###   ########.fr       */
+/*   Updated: 2024/12/08 20:15:55 by samberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ int generate_rgb()
 {
 	// get all the rgb color from the gratient each time i call the function
 	static int i = 0;
-	int r = (int)(sin(0.005 * i + 0) * 127 + 128);
-	int g = (int)(sin(0.005 * i + 2) * 127 + 128);
-	int b = (int)(sin(0.005 * i + 4) * 127 + 128);
+	int r = (int)(sin(0.01 * i + 0) * 127 + 128);
+	int g = (int)(sin(0.01 * i + 2) * 127 + 128);
+	int b = (int)(sin(0.01 * i + 4) * 127 + 128);
 	i++;
 	return create_trgb(0, r, g, b);
 }
