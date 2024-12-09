1aa5dbd3ae1392f9bf4344a3c41157739e2e4dec
hallfana <hallfana@proton.me>
2024-12-09 03:19:43 +0100

    auto

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: samberna <samberna@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 17:46:48 by hallfana          #+#    #+#             */
-/*   Updated: 2024/12/09 03:19:30 by samberna         ###   ########.fr       */
+/*   Updated: 2024/12/09 03:19:42 by samberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -177,7 +177,7 @@ int	main(int argc, char **argv)
 			my_mlx_pixel_put(
 				&img,
 				isometric_format_x(tab.tab[i][j].x, tab.tab[i][j].y, tab.tab[i][j].z) + WINX / 2,
-				isometric_format_y(tab.tab[i][j].x, tab.tab[i][j].y, tab.tab[i][j].z) + WINY / 2+200,
+				isometric_format_y(tab.tab[i][j].x, tab.tab[i][j].y, tab.tab[i][j].z) + WINY / 2-400,
 				tab.tab[i][j].color
 			);
 		}
