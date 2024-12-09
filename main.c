94e29df85083b01c936985d0d3e470d3912534ec
hallfana <hallfana@proton.me>
2024-12-09 02:36:19 +0100

    auto

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: samberna <samberna@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 17:46:48 by hallfana          #+#    #+#             */
-/*   Updated: 2024/12/09 02:36:05 by samberna         ###   ########.fr       */
+/*   Updated: 2024/12/09 02:36:18 by samberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -201,7 +201,6 @@ int	main(int argc, char **argv)
             int y1 = isometric_format_y(tab.tab[m][n].x, tab.tab[m][n].y, tab.tab[m][n].z) + WINY / 2;
             int x2 = isometric_format_x(tab.tab[m][n + 1].x, tab.tab[m][n + 1].y, tab.tab[m][n + 1].z) + WINX / 2;
             int y2 = isometric_format_y(tab.tab[m][n + 1].x, tab.tab[m][n + 1].y, tab.tab[m][n + 1].z) + WINY / 2;
-            printf("Drawing line from (%d, %d) to (%d, %d)\n", x1, y1, x2, y2);
             my_mlx_line_put(img, x1, y1, x2, y2);
         }
         /*if (m < tab.height - 1)
@@ -210,7 +209,6 @@ int	main(int argc, char **argv)
             int y1 = isometric_format_y(tab.tab[m][n].x, tab.tab[m][n].y, tab.tab[m][n].z) + WINY / 2;
             int x2 = isometric_format_x(tab.tab[m + 1][n].x, tab.tab[m + 1][n].y, tab.tab[m + 1][n].z) + WINX / 2;
             int y2 = isometric_format_y(tab.tab[m + 1][n].x, tab.tab[m + 1][n].y, tab.tab[m + 1][n].z) + WINY / 2;
-            printf("Drawing line from (%d, %d) to (%d, %d)\n", x1, y1, x2, y2);
             my_mlx_line_put(img, x1, y1, x2, y2);
         }*/
     }
