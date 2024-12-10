b2ecfcf9bff453dca7b2c1d53e952ad602dd0d01
hallfana <hallfana@proton.me>
2024-12-10 19:00:03 +0100

    auto

--- src/init/tab_populate.c
+++ src/init/tab_populate.c
@@ -6,7 +6,7 @@
 /*   By: samberna <samberna@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 21:25:23 by samberna          #+#    #+#             */
-/*   Updated: 2024/12/10 18:59:38 by samberna         ###   ########.fr       */
+/*   Updated: 2024/12/10 19:00:00 by samberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,10 @@ static void	_fdf_line_populate(t_fdf *fdf, char *line, int i)
 		fdf->tab[i][j].draw_y = 0;
 		fdf->tab[i][j].draw_z = 0;
 		if (tmp[1])
+		{
+			printf("color: %s\n", tmp[1]);
 			fdf->tab[i][j].color = _fdf_atoi_base(tmp[1] + 2, "0123456789ABCDEF");
+		}
 		else
 			fdf->tab[i][j].color = 0xFFFFFF;
 		j++;
