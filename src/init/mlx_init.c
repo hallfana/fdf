97a7028a370b86a5a5d220bc8ec8630aed45b09e
hallfana <hallfana@proton.me>
2024-12-10 19:04:20 +0100

    auto

--- src/init/mlx_init.c
+++ src/init/mlx_init.c
@@ -6,7 +6,7 @@
 /*   By: samberna <samberna@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 20:47:33 by samberna          #+#    #+#             */
-/*   Updated: 2024/12/10 00:54:46 by samberna         ###   ########.fr       */
+/*   Updated: 2024/12/10 19:04:17 by samberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,5 @@ void	_fdf_mlx_init(t_fdf *fdf)
 {
 	fdf->mlx = mlx_init();
 	fdf->win = mlx_new_window(fdf->mlx, _FDF_WIDTH, _FDF_HEIGHT, "FDF");
-	fdf->zoom = 0.3;
+	fdf->zoom = 0.8;
 }
