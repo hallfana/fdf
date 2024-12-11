3aa7101df770528679dae234b28d04bcdb70c8ad
Samuel Bernard <samberna@c3r3p7.42perpignan.fr>
2024-12-11 13:56:11 +0100

    auto

--- src/init/tab_init.c
+++ src/init/tab_init.c
@@ -6,7 +6,7 @@
 /*   By: samberna <samberna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 20:51:22 by samberna          #+#    #+#             */
-/*   Updated: 2024/12/11 13:53:29 by samberna         ###   ########.fr       */
+/*   Updated: 2024/12/11 13:56:06 by samberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,6 +51,7 @@ static int	_fdf_tab_count_columns(char *file)
 {
 	int		fd;
 	int		columns;
+	int		current_columns;
 	char	*line;
 	char	**split_line;
 
@@ -71,7 +72,7 @@ static int	_fdf_tab_count_columns(char *file)
 	while (line)
 	{
 		split_line = ft_split(line, ' ');
-		int current_columns = 0;
+		current_columns = 0;
 		while (split_line[current_columns])
 			current_columns++;
 		if (columns != current_columns)
@@ -92,7 +93,7 @@ static int	_fdf_tab_count_columns(char *file)
 
 void	_fdf_tab_init(t_fdf *fdf, char *file)
 {
-	int i;
+	int	i;
 
 	fdf->lines = _fdf_tab_count_lines(file);
 	fdf->columns = _fdf_tab_count_columns(file);
@@ -107,9 +108,9 @@ void	_fdf_tab_init(t_fdf *fdf, char *file)
 		ft_putstr_fd("Error: Malloc Failed\n", 1);
 		exit(0);
 	}
-	i = 0;
-	while (i < fdf->lines && (fdf->tab[i] = (t_point *)malloc(sizeof(t_point) * fdf->columns)))
-		i++;
+	i = -1;
+	while (++i < fdf->lines)
+		fdf->tab[i] = (t_point *)malloc(sizeof(t_point) * fdf->columns);
 	if (i != fdf->lines)
 	{
 		ft_putstr_fd("Error: Malloc Failed\n", 1);
