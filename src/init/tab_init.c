70e380b5cd6605a31c8a5093fe71205f98625da7
Samuel Bernard <samberna@c3r3p7.42perpignan.fr>
2024-12-11 14:00:13 +0100

    auto

--- src/init/tab_init.c
+++ src/init/tab_init.c
@@ -6,7 +6,7 @@
 /*   By: samberna <samberna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 20:51:22 by samberna          #+#    #+#             */
-/*   Updated: 2024/12/11 13:56:06 by samberna         ###   ########.fr       */
+/*   Updated: 2024/12/11 14:00:13 by samberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,6 @@ static int	_fdf_tab_count_columns(char *file)
 {
 	int		fd;
 	int		columns;
-	int		current_columns;
 	char	*line;
 	char	**split_line;
 
@@ -67,24 +66,6 @@ static int	_fdf_tab_count_columns(char *file)
 			columns++;
 		free(line);
 		_fdf_free_split(split_line);
-	}
-	line = _gnl_get_next_line(fd);
-	while (line)
-	{
-		split_line = ft_split(line, ' ');
-		current_columns = 0;
-		while (split_line[current_columns])
-			current_columns++;
-		if (columns != current_columns)
-		{
-			free(line);
-			_fdf_free_split(split_line);
-			close(fd);
-			return (0);
-		}
-		free(line);
-		_fdf_free_split(split_line);
-		line = _gnl_get_next_line(fd);
 	}
 	free(line);
 	close(fd);
