1d97e372773f6cc255ebc7616ccbb95d14dc2ac3
Samuel Bernard <samberna@c3r3p7.42perpignan.fr>
2024-12-11 14:42:53 +0100

    auto

--- src/hook/keyboard.c
+++ src/hook/keyboard.c
@@ -6,7 +6,7 @@
 /*   By: samberna <samberna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 00:04:44 by samberna          #+#    #+#             */
-/*   Updated: 2024/12/11 14:34:40 by samberna         ###   ########.fr       */
+/*   Updated: 2024/12/11 14:42:50 by samberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 int	_fdf_key_hook(int keycode, t_fdf *fdf)
 {
+	printf("keycode: %d\n", keycode);
 	if (keycode == 65362)
 	{
 		fdf->zoom *= 1.1;
