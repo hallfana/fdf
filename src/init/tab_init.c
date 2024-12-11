19c0c4d03d5ab1685213d5688789ea4557cce000
Samuel Bernard <samberna@c3r3p7.42perpignan.fr>
2024-12-11 14:30:57 +0100

    auto

--- src/init/tab_init.c
+++ src/init/tab_init.c
@@ -6,7 +6,7 @@
 /*   By: samberna <samberna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 20:51:22 by samberna          #+#    #+#             */
-/*   Updated: 2024/12/11 14:29:49 by samberna         ###   ########.fr       */
+/*   Updated: 2024/12/11 14:30:55 by samberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -111,7 +111,6 @@ static int	_fdf_tab_count_columns(char *file)
 		free(line);
 		_fdf_free_split(split_line);
 	}
-	//close(fd);
 	return (columns);
 }
 
