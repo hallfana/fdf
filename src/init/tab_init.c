1491b65712d85d93678b177eb43b4236951fd05d
hallfana <hallfana@proton.me>
2024-12-12 18:28:31 +0100

    auto/dev

--- src/init/tab_init.c
+++ src/init/tab_init.c
@@ -6,7 +6,7 @@
 /*   By: samberna <samberna@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 20:51:22 by samberna          #+#    #+#             */
-/*   Updated: 2024/12/12 18:18:16 by samberna         ###   ########.fr       */
+/*   Updated: 2024/12/12 18:28:29 by samberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,6 +67,7 @@ static int	_fdf_tab_count_columns(char *file)
 		_fdf_free_split(split_line);
 	}
 	free(line);
+	close(fd);
 	return (columns);
 }
 
