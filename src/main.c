3e39ce0d0a7af19bb066d1a60108144efc52391d
hallfana <hallfana@proton.me>
2024-12-12 18:10:53 +0100

    auto/dev

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: samberna <samberna@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 20:35:39 by samberna          #+#    #+#             */
-/*   Updated: 2024/12/12 18:07:45 by samberna         ###   ########.fr       */
+/*   Updated: 2024/12/12 18:10:50 by samberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,13 @@ int	main(int argc, char **argv)
 	_fdf_mlx_init(&fdf);
 	_fdf_tab_init(&fdf, argv[1]);
 	_fdf_tab_populate(&fdf, argv[1]);
+	fdf.img = malloc(sizeof(t_img));
+	fdf.img->img = mlx_new_image(fdf.mlx, _FDF_WIDTH, _FDF_HEIGHT);
+	if (!fdf.img->img)
+	{
+		ft_putstr_fd("Error: Failed to create new image\n", 1);
+		return (0);
+	}
 	_fdf_calculate_draw(&fdf, &_fdf_calculate_iso);
 	_fdf_display(&fdf);
 	mlx_hook(fdf.win, 2, 1L << 0, _fdf_key_hook, &fdf);
